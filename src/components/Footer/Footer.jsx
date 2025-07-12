fc1a977ea6868f60a2dc1b307fe0333ac166ef49
Aniket Tiwari <pt5602238@gmail.com>
2025-07-12 10:55:11 +0530

    Update Footer.jsx
    up

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,36 +1,41 @@
-import React from 'react'
-import './Footer.css'
-import user_icon from '../../assets/user_icon.svg'  
+import React from 'react';
+import './Footer.css';
+import user_icon from '../../assets/user_icon.svg';
 
-
-const  Footer= () => {
+const Footer = () => {
   return (
-    <div className='footer'>
-        <div className="footer-top">
-            <div className="footer-top-left">
-                {/* <img src={footer_logo} alt="" /> */}
-                <p>I am a full stack web developer from Lucknow, Uttar pradesh with 1 year of experience </p>
-            </div>
-            <div className="footer-top-right">
-                <div className="footer-email-input">
-                    {/* user icon jo enail ke andar dikh rha hai wohai  */}
-                    <img src={user_icon} alt="" />
-                    <input type="email" placeholder="Enter your email" />
-                </div>
-                <div className="footer-subscribe">Subscribe</div>
-            </div>
+    <div className="footer">
+      <div className="footer-top">
+        <div className="footer-top-left">
+          <p>
+            I am a full stack web developer from Lucknow, Uttar Pradesh with 1 year of experience.
+          </p>
         </div>
-         <hr />
-         <div className="footer-bottom">
-            <p className="footer-bottom-left">©️ 2025 Aniket Tiwari  All rights are reserved </p>
-            <div className="footer-bottom-right">
-                <p>Term of Services</p>
-                <p>Privacy Policy</p>
-                <p>Connect With me </p>
-            </div>
-         </div>
-    </div>
-  )
-}
 
-export default Footer
+        <div className="footer-top-right">
+          <div className="footer-email-input">
+            <img src={user_icon} alt="User Icon" />
+            <input type="email" placeholder="Enter your email" />
+          </div>
+            <button className="footer-subscribe">Subscribe</button>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="footer-bottom">
+        <p className="footer-bottom-left">
+          © 2025 Aniket Tiwari. All rights reserved.
+        </p>
+
+        <div className="footer-bottom-right">
+          <p>Terms of Service</p>
+          <p>Privacy Policy</p>
+          <p>Connect with Me</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

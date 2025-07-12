0b232188ca346c0bbff62692a12099ee96a15ac8
Aniket Tiwari <pt5602238@gmail.com>
2025-07-12 10:35:01 +0530

    Update Footer.jsx
    GAP

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const  Footer= () => {
         <div className="footer-top">
             <div className="footer-top-left">
                 {/* <img src={footer_logo} alt="" /> */}
-                <p>I am a full stack web developer from Lucknow, Uttar pradesh with 1 year of experience </p>
+                <p>I am a full stack web developer from Lucknow, Uttar pradesh with 2 year of experience </p>
             </div>
             <div className="footer-top-right">
                 <div className="footer-email-input">
@@ -23,7 +23,7 @@ const  Footer= () => {
         </div>
          <hr />
          <div className="footer-bottom">
-            <p className="footer-bottom-left">©️ 2025 Aniket TiwariAll rights are reserved </p>
+            <p className="footer-bottom-left">©️ 2025 Aniket Tiwari  All rights are reserved </p>
             <div className="footer-bottom-right">
                 <p>Term of Services</p>
                 <p>Privacy Policy</p>
